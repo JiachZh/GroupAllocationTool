9212341e373538c25bb5837b618cb7bd9298c2bd
Helen Shepherd <ShepherdHA1@Cardiff.ac.uk>
2021-05-05 12:40:37 +0100

    forms.py option form updated

--- blog/forms.py
+++ blog/forms.py
@@ -46,7 +46,7 @@ class QuestionnaireForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class OptionForm(FlaskForm):
-    option1 = RadioField('Prior programming experience', choices=[('1', 'low'),('2','medium'),('3', 'high')])
-    option2 = RadioField('Hold a STEM Degree', choices=[('1', 'low'),('2','medium'),('3', 'high')])
-    option3 = RadioField('Gender', choices=[('1', 'low'),('2','medium'),('3', 'high')])
+    option1 = RadioField("1. Students' level of programming experience", choices=[('1', '1'),('2','2'),('3', '3')])
+    option2 = RadioField('2. Whether students hold a STEM Degree', choices=[('1', '1'),('2','2'),('3', '3')])
+    option3 = BooleanField('3. Would you rather avoid having students being the only male or female in their team? Select if yes.')
     groupSize = RadioField('Select the number of students in each group', validators = [InputRequired()], choices=[('5', '5'), ('6', '6')])
